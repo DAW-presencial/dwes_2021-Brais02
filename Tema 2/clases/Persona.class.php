deaf57b62fe870513d7ef2bf92aa35f2d801295d
Brais02 <74008056+Brais02@users.noreply.github.com>
2021-10-27 17:57:14 +0200

    Introduciendo la prueba de crear un mapa de clases

---
+++ Tema 2/clases/Persona.class.php
@@ -0,0 +1,5 @@
+<?php
+class Persona {
+public $numero = 1;
+public $casa = 5;
+}
---
+++ Tema 2/clases/Pisos.php
@@ -0,0 +1,10 @@
+<?php
+
+class Pisos {
+public $numero = 1;
+public $casa = 5;
+}
+
+class Casas extends Pisos {
+    
+}
